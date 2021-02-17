fc56a2b73c81615988425521c279cf57d0a62112
Valentyn Saik <svbackend22@gmail.com>
2021-02-17 12:03:28 +0200

    Updates

--- precache-manifest.b7669f07f839c3423840b0cc6f2864be.js
+++ precache-manifest.b7669f07f839c3423840b0cc6f2864be.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "ccdbaebfacdf91900dd5",
-    "url": "/static/js/main.ccdbaebf.chunk.js"
+    "revision": "48d07d15c1de1c841494",
+    "url": "/static/js/main.48d07d15.chunk.js"
   },
   {
     "revision": "4a34e45d34cafb7623bd",
     "url": "/static/js/1.4a34e45d.chunk.js"
   },
   {
-    "revision": "ccdbaebfacdf91900dd5",
+    "revision": "48d07d15c1de1c841494",
     "url": "/static/css/main.838b0440.chunk.css"
   },
   {
-    "revision": "1bf82547baaafdbbf241ee9711ed16cb",
+    "revision": "36bb5b8eddb88471e382cb899e79f2c1",
     "url": "/index.html"
   }
 ];
